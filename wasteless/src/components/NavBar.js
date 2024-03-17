1cc0fc1ce55d2d7644a3461e6f0992bbff00e0f7
Khush Kothari <56465845+khushk21@users.noreply.github.com>
2024-03-17 16:38:55 +0800

    Added navbar

---
+++ wasteless/src/components/NavBar.js
@@ -0,0 +1,99 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+import Badge from "@mui/material/Badge";
+import Shield from "@mui/icons-material/Shield";
+import "./styles/NavBar.css";
+
+const Navbar = ({ points }) => {
+  return (
+    <div>
+      <AppBar position="fixed" style={{ backgroundColor: "#006400" }}>
+        <Toolbar>
+          <NavLink
+            to="/"
+            exact
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+              fontSize: "20px",
+            }}
+          >
+            WasteLess
+          </NavLink>
+
+          <Box flexGrow={1} />
+          <NavLink
+            to="/"
+            exact
+            className="navLink"
+            style={{
+              color: "inherit",
+              textDecoration: "none",
+              marginRight: "20px",
+              fontSize: "20px",
+            }}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/Catalogue"
+            className="navLink"
+            style={{
+              color: "inherit",
+              textDecoration: "none",
+              marginRight: "20px",
+              fontSize: "20px",
+            }}
+          >
+            Catalogue
+          </NavLink>
+          <NavLink
+            to="/addwaste"
+            className="navLink"
+            style={{
+              color: "inherit",
+              textDecoration: "none",
+              marginRight: "20px",
+              fontSize: "20px",
+            }}
+          >
+            Add Waste
+          </NavLink>
+          <NavLink
+            to="/favourite"
+            className="navLink"
+            style={{
+              color: "inherit",
+              textDecoration: "none",
+              marginRight: "20px",
+              fontSize: "20px",
+            }}
+          >
+            Favourite
+          </NavLink>
+          <NavLink
+            to="/profile"
+            className="navLink"
+            style={{
+              color: "inherit",
+              textDecoration: "none",
+              marginRight: "20px",
+              fontSize: "20px",
+            }}
+          >
+            Profile
+          </NavLink>
+
+          <Badge badgeContent={points} color="secondary">
+            <Shield />
+          </Badge>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Navbar;
--- wasteless/src/pages/AddNewWaste.js
+++ wasteless/src/pages/AddNewWaste.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-
+import Navbar from "../components/NavBar";
 
 const AddNewWaste = () => {
   return (
@@ -16,46 +16,45 @@ const AddNewWaste = () => {
         justifyContent: "center",
       }}
     >
-
+      <Navbar points={"0"} />
       <Card style={{ maxWidth: 400 }}>
         <CardContent>
           <Typography
             variant="h4"
             style={{ textAlign: "center", color: "#8bc34a" }}
-          >Wasteless
-            
+          >
+            Wasteless
           </Typography>
-           
-            <div style = {{fontSize: "1.5em" ,color: "darkgreen" }}>Add Waste</div><br></br>
-            <u>Record details of recent waste disposal</u>
-            <br/>
-            <br/>
-            <div style = {{ width:"fit-content"}}>
-            Date : <input type = "date" name = "date"></input>
-            <br/>
-            <br/>
-            Time : <input type = "time" name = "time"></input>
-            <br/>
-            <br/>
-            Enter Weight : <input type = "text" name ="weight"></input> Kg
-            <br/>
-            <br/>
+
+          <div style={{ fontSize: "1.5em", color: "darkgreen" }}>Add Waste</div>
+          <br></br>
+          <u>Record details of recent waste disposal</u>
+          <br />
+          <br />
+          <div style={{ width: "fit-content" }}>
+            Date : <input type="date" name="date"></input>
+            <br />
+            <br />
+            Time : <input type="time" name="time"></input>
+            <br />
+            <br />
+            Enter Weight : <input type="text" name="weight"></input> Kg
+            <br />
+            <br />
             Waste Category :&nbsp;
-                <select name="category">
-                    <option value="category1">Category 1</option>
-                    <option value="category2">Category 2</option>
-                </select>
-            </div>
+            <select name="category">
+              <option value="category1">Category 1</option>
+              <option value="category2">Category 2</option>
+            </select>
+          </div>
 
-            <br/>
-            <br/>
-            <div style = {{ textAlign: "center"}}>
-                <button style = {{ backgroundColor: "#8bc34a" }}>Add Record</button>
-            </div>
-            </CardContent>
+          <br />
+          <br />
+          <div style={{ textAlign: "center" }}>
+            <button style={{ backgroundColor: "#8bc34a" }}>Add Record</button>
+          </div>
+        </CardContent>
       </Card>
-
-     
     </div>
   );
 };
