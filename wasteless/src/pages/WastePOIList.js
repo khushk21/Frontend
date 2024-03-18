635cba89ec485f491fd1d3f9cbd39743aa27af2f
Khush Kothari <56465845+khushk21@users.noreply.github.com>
2024-03-18 22:38:18 +0800

    wastepoi list page done

--- wasteless/src/pages/WastePOIList.js
+++ wasteless/src/pages/WastePOIList.js
@@ -1,11 +1,42 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import WastePOICard from "../components/WastePOICard";
+import Navbar from "../components/NavBar";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 const WastePOIList = () => {
   const location = useLocation();
-  const username = location.state.user.userName;
-  console.log(username);
-  return <div>{username}</div>;
+  const { user, wastePOIs, category } = location.state;
+  return wastePOIs ? (
+    <div
+      className="wastepoilist-container"
+      style={{
+        backgroundColor: "#8bc34a",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "20px",
+      }}
+    >
+      <Navbar
+        points={user ? user.points : 0}
+        username={user ? user.userName : ""}
+      />
+      <h1 style={{ textAlign: "center" }}>Nearest {category} WastePOIs</h1>
+      <Box width="90%" m={2}>
+        <Grid container spacing={3} justifyContent="center">
+          {wastePOIs.map((poi, index) => (
+            <WastePOICard key={index} poi={poi} user={user} />
+          ))}
+        </Grid>
+      </Box>
+    </div>
+  ) : (
+    <div>No waste POIs found</div>
+  );
 };
 
 export default WastePOIList;
