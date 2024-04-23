9b12e87941204357b20a58613263180d1fcd9085
Khush Kothari <56465845+khushk21@users.noreply.github.com>
2024-04-23 21:48:20 +0800

    changes

--- wasteless/src/components/NavBar.test.js
+++ wasteless/src/components/NavBar.test.js
@@ -18,19 +18,28 @@ describe("Navbar", () => {
     expect(getByText("WasteLess")).toBeInTheDocument();
 
     // Assert that the Home link is rendered correctly
-    expect(getByText("Home").closest("a")).toHaveAttribute("href", `/home?username=${username}`);
+    expect(getByText("Home").closest("a")).toHaveAttribute(
+      "href",
+      `/home?username=${username}`
+    );
 
     // Assert that the Catalogue link is rendered correctly
-    expect(getByText("Catalogue").closest("a")).toHaveAttribute("href", `/catalogue?username=${username}`);
+    expect(getByText("Catalogue").closest("a")).toHaveAttribute(
+      "href",
+      `/catalogue?username=${username}`
+    );
 
     // Assert that the Add Waste link is rendered correctly
-    expect(getByText("Add Waste").closest("a")).toHaveAttribute("href", `/addwaste?username=${username}`);
-
-    // Assert that the Favourite link is rendered correctly
-    expect(getByText("Favourite").closest("a")).toHaveAttribute("href", "/favourite");
+    expect(getByText("Add Waste").closest("a")).toHaveAttribute(
+      "href",
+      `/addwaste?username=${username}`
+    );
 
     // Assert that the Profile link is rendered correctly
-    expect(getByText("Profile").closest("a")).toHaveAttribute("href", `/profile?username=${username}`);
+    expect(getByText("Profile").closest("a")).toHaveAttribute(
+      "href",
+      `/profile?username=${username}`
+    );
 
     // Assert that the points are rendered correctly
     expect(getByText("100")).toBeInTheDocument();
